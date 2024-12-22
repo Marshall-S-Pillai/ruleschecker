0f5af0e498e882ba0a1ec935f27eb8824d582b3c
nilesh1114 <nileshkr1114@gmail.com>
2024-12-23 02:14:15 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,11 @@
+# backend.tf - Root-level backend configuration
+
+terraform {
+  backend "s3" {
+    bucket         = "powertool1107"
+    key            = "rules/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "powertool"
+  }
+}
