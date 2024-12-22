95f60ff174b108d0f991d6a909e984fa42f25448
nilesh1114 <nileshkr1114@gmail.com>
2024-12-22 18:07:59 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+# main.tf
+
+# Configure AWS provider
+provider "aws" {
+  region = var.aws_region
+}
+
+# EC2 Instance resource
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.environment == "production" ? var.production_instance_type : var.staging_instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name = "WebServer-${var.environment}"
+  }
+}
+
+# Output the public IP of the EC2 instance
+output "instance_ip" {
+  value = aws_instance.web.public_ip
+}
